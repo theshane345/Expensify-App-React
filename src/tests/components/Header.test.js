c4c8b0630f85558a51ee68bb853b1acc905d75de
theshane345 <56794959+theshane345@users.noreply.github.com>
2020-02-05 20:38:54 +0000

    bEFORE JSON

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,14 @@
-import ReactShallowRenderer from  'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Header from '../../components/Header';
 
 
 test('should render header' , () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+
+    //expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
